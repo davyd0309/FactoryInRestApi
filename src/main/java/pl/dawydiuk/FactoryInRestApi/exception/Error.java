aa764dae5956da5e5cda6f4435ca9d625e0c737b
Konrad <konrad.dawydiuk@gmail.com>
2019-10-01 21:12:49 +0200

    some refactor

---
+++ src/main/java/pl/dawydiuk/FactoryInRestApi/exception/Error.java
@@ -0,0 +1,24 @@
+package pl.dawydiuk.FactoryInRestApi.exception;
+
+public class Error {
+
+    private String code;
+    private String message;
+
+    public Error(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static Error buildError(String code, String message){
+        return new Error(code,message);
+    }
+}
